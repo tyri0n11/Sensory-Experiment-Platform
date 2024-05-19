48adecea15c369cdf4a92672730771b27b6fbe88
Châu Thịnh <chauthinh00710@gmail.com>
2024-05-19 09:10:04 +0700

    initialization: first touch

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module main.sensoryexpeirmentplatform {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens main.sensoryexpeirmentplatform to javafx.fxml;
+    exports main.sensoryexpeirmentplatform;
+}
