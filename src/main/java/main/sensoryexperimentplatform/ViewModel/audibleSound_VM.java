a82f7a7e789a1aa1ea1cbf5925a7e875744df443
quynhanhhoang572004 <quynhanhhoang572004@gmail.com>
2024-05-28 09:29:43 +0700

    change but lack of model

---
+++ src/main/java/main/sensoryexperimentplatform/ViewModel/audibleSound_VM.java
@@ -0,0 +1,10 @@
+package main.sensoryexperimentplatform.ViewModel;
+
+import javafx.beans.property.StringProperty;
+
+public class audibleSound_VM {
+    private StringProperty title;
+    private StringProperty content;
+    private StringProperty helpText;
+    private StringProperty buttonText;gi
+}
