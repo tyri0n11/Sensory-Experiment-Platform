af1d12fa81deb00289f3ed2bfe9b2d24b9baa046
httgiang <httgiang.gdsciu@gmail.com>
2024-06-12 03:18:57 +0700

    UI adjustments

--- src/main/java/main/sensoryexperimentplatform/controllers/Main.java
+++ src/main/java/main/sensoryexperimentplatform/controllers/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/main/sensoryexperimentplatform/Base.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/main/sensoryexperimentplatform/testresponsive.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setTitle("Sense XP");
